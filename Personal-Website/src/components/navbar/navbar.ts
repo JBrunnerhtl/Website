264657c042d7f8a9b70b4c4689bb965be341ab18
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-06-16 21:11:19 +0200

    added unstyled navbar

---
+++ Personal-Website/src/components/navbar/navbar.ts
@@ -0,0 +1,33 @@
+import {AfterViewInit, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar implements AfterViewInit {
+  links = [
+    {name: 'Home', url: '/'},
+    {name: 'About', url: '/about'},
+    {name: 'Contact', url: '/contact'},
+    ];
+
+    initDropDown() {
+      const dropdown = document.querySelector('.dropdown-menu');
+      if(!dropdown) return;
+      this.links.forEach(link => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = link.url;
+        a.textContent = link.name;
+        a.className = 'dropdown-item';
+        li.appendChild(a);
+        dropdown.appendChild(li);
+      })
+    }
+
+  ngAfterViewInit() {
+      this.initDropDown();
+  }
+}
