2abd1e47cd89823910d336abd8a53d4a03c2563c
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-06-20 11:51:15 +0200

    try

--- Personal-Website/src/components/timeline/timeline.ts
+++ Personal-Website/src/components/timeline/timeline.ts
@@ -14,10 +14,11 @@ export class Timeline implements AfterViewInit{
     {text: "first job at 16", year: "2024"},
     {text: "graduated high school at 18", year: "2025"},
   ];
-  @ViewChild('container', { static: true }) containerRef!: ElementRef;
+  @ViewChild('container', { static: false }) containerRef!: ElementRef;
   loadInCards() {
 
     if (typeof document === 'undefined' || typeof window === 'undefined') return;
+    console.log(this.containerRef.nativeElement);
     const outerContainer = this.containerRef.nativeElement;
     if (!outerContainer) return;
     const container = document.createElement('div');
@@ -33,11 +34,12 @@ export class Timeline implements AfterViewInit{
     let count = 0; // Counter of the position in the infos array
     let divisor = 2; // Divisor to determine the border position
     let i = 0; // Counter to check on which side I am currently
+    const windowSize = window.innerWidth; // Get the current window size
     // Create a new div for each info and add it to the container
     while (count < this.infos.length) {
       const createdDiv = document.createElement('div');
       createdDiv.className = 'col';
-      console.log(count)
+
       if (i % divisor === 0 || window.innerWidth < 768) {
         createdDiv.innerHTML =
           `
@@ -55,22 +57,26 @@ export class Timeline implements AfterViewInit{
 
       }
 
-      if (window.innerWidth >= 768 && (i+1) % 2 !== 0) {
+      if (windowSize >= 768 && (i + 1) % 2 !== 0) {
         createdDiv.className += ' border-right';
         createdDiv.style.animation = 'fly-in-from-right 2s ease-in-out forwards';
         console.log("right")
-      }
-      else if(window.innerWidth >= 768) {
+      } else if (windowSize >= 768) {
         createdDiv.className += ' border-left';
         createdDiv.style.animation = 'fly-in-from-left 2s ease-in-out forwards';
         console.log("left")
-      }
-      else
-      {
-        createdDiv.style.borderTop = '2px solid #800ac4';
+      } else {
+        createdDiv.className += ' border-top';
+        if ((i + 1) % 2 === 0) {
+          createdDiv.style.animation = 'fly-in-from-right 2s ease-in-out forwards';
+
+        } else {
+          createdDiv.style.animation = 'fly-in-from-left 2s ease-in-out forwards';
+        }
       }
 
-      if(count >= this.infos.length && window.innerWidth < 768) { // It is for mobile if it is the last element than add a bottom border
+
+      if (count >= this.infos.length && windowSize < 768) { // It is for mobile if it is the last element than add a bottom border
         createdDiv.className += ' border-bottom';
       }
       container?.appendChild(createdDiv);
@@ -78,20 +84,19 @@ export class Timeline implements AfterViewInit{
 
     }
     // Add an extra container so that both sides have a border and not only the left or the right side
-    const extraContainer = document.createElement('div');
-    extraContainer.className = 'col';
-    if(i % 2 !== 0 && window.innerWidth >= 768) {
-      extraContainer.style.borderLeft = '2px solid #800ac4';
-      extraContainer.style.animation = 'fly-in-from-left 2s ease-in-out forwards';
+    if (windowSize >= 768) {
+      const extraContainer = document.createElement('div');
+      extraContainer.className = 'col';
+      if (i % 2 !== 0) {
+        extraContainer.className += ' border-left';
+        extraContainer.style.animation = 'fly-in-from-left 2s ease-in-out forwards';
+      } else {
+        extraContainer.className += ' border-right';
+        extraContainer.style.animation = 'fly-in-from-right 2s ease-in-out forwards';
+      }
+      container?.appendChild(extraContainer);
     }
-    else
-    {
-      extraContainer.style.borderRight = '2px solid #800ac4';
-      extraContainer.style.animation = 'fly-in-from-right 2s ease-in-out forwards';
-    }
-    container?.appendChild(extraContainer);
   }
-
   ngAfterViewInit(): void {
     this.loadInCards();
   }
